5ff6309209260f601376ff1b2dd5d9173f7b8efd
sorude26 <sorude26@gmail.com>
2021-09-24 20:16:45 +0900

    キャラクター操作用スクリプトを追加した

---
+++ Assets/Scripts/Character/CharacterControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace victory7
+{
+    public abstract class CharacterControl : MonoBehaviour
+    {
+        protected int m_maxHP = default;
+        public int CurrentHP { get; protected set; }
+        public abstract void CharacterUpdate();
+        public virtual void Damage(int damage)
+        {
+            CurrentHP -= damage;
+            if (CurrentHP <= 0)
+            {
+                CurrentHP = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Character/EnemyControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace victory7
+{
+    public class EnemyControl : CharacterControl
+    {
+        [SerializeField]
+        protected EnemyParameter m_parameter = default;
+        protected float m_rbtGauge;
+        public void StartSet()
+        {
+            m_maxHP = m_parameter.MaxHP;
+            CurrentHP = m_parameter.MaxHP;
+        }
+        public override void CharacterUpdate()
+        {
+            
+        }
+    } 
+}
