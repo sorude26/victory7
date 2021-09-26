1ba04c9e3ec1351c2124d38816ad9cd6b80290fc
sorude26 <sorude26@gmail.com>
2021-09-26 12:06:47 +0900

    プレイヤーの仮アニメーションを追加した

---
+++ Assets/Scripts/Character/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace victory7
+{
+    public class TestController : MonoBehaviour
+    {
+        [SerializeField]
+        EnemyControl enemy;
+        void Start()
+        {
+            enemy.StartSet();
+        }
+
+        void Update()
+        {
+            enemy.CharacterUpdate();
+        }
+    }
+}
