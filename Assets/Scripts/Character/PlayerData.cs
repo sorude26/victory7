cf493cb6a6a964a55e44fa7c8aeb7be2a079520b
sorude26 <sorude26@gmail.com>
2021-10-10 11:35:21 +0900

    パラメータ保存用スクリプトを追加した

---
+++ Assets/Scripts/Character/PlayerData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace victory7
+{
+    public class PlayerData
+    {
+        public static int CurrentHP { get; protected set; }
+        public static int CurrentSP { get; protected set; }
+        public static int CurrentGP { get; protected set; }
+        public static void StartSet(PlayerParameter parameter)
+        {
+            CurrentHP = parameter.MaxHP;
+            CurrentSP = 0;
+            CurrentGP = 0;
+        }
+        public static void SetData(int hp,int sp,int gp)
+        {
+            CurrentHP = hp;
+            CurrentSP = sp;
+            CurrentGP = gp;
+        }
+    }
+}
