07911e2762f15a5f0d1ac2070754ea276425a3da
sorude26 <sorude26@gmail.com>
2021-11-03 14:54:33 +0900

    エフェクトの再生を行う機能を追加した

--- Assets/Scripts/Battle/EffectManager.cs
+++ Assets/Scripts/Battle/EffectManager.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 namespace victory7
 {
+    public enum EffectType
+    {
+        Heel,
+        Chage,
+        Guard,
+        Damage1,
+        Damage2,
+        Damage3,
+    }
     public class EffectManager : MonoBehaviour
     {
         public static EffectManager Instance { get; private set; }
         [SerializeField]
         ViewText m_viewText = default;
+        [SerializeField]
+        GameObject[] m_effectPrefabs = default;
         public ViewText Text { get => m_viewText; }
         private void Awake()
         {
             Instance = this;
+        }
+        public void PlayEffect(EffectType type, Vector2 pos)
+        {
+            Instantiate(m_effectPrefabs[(int)type], transform).transform.position = pos;
         }
     }
 }
--- Assets/Scripts/Character/CharacterControl.cs
+++ Assets/Scripts/Character/CharacterControl.cs
@@ -19,8 +19,9 @@ namespace victory7
             CurrentHP -= damage;
             Debug.Log($"{gameObject.name}は{damage}のダメージ");
             var view = Instantiate(EffectManager.Instance.Text);
-            view.transform.position = this.transform.position;
+            view.transform.position = this.transform.position + Vector3.up;
             view.View("-" + damage.ToString(), Color.red);
+            EffectManager.Instance.PlayEffect(EffectType.Damage1, transform.position);
             if (CurrentHP <= 0)
             {
                 CurrentHP = 0;
