d5c07848c8ce24e50ed10ea9082dfa5c0dc33e07
sorude26 <sorude26@gmail.com>
2021-11-03 11:26:25 +0900

    α版用のシーンを作成、スクリプトを設定した

---
+++ Assets/Scripts/ResultControlα.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace victory7
+{
+    public class ResultControlα : MonoBehaviour
+    {
+        bool m_start = false;
+        void Start()
+        {
+            FadeController.Instance.StartFadeIn();
+        }
+
+        void Update()
+        {
+            if (m_start)
+            {
+                return;
+            }
+            if (Input.GetButtonDown("Jump"))
+            {
+                ReturnTitle();
+            }
+        }
+        public void ReturnTitle()
+        {
+            if (m_start)
+            {
+                return;
+            }
+            m_start = true;
+            FadeController.Instance.StartFadeOut(LoadTitle);
+        }
+        void LoadTitle()
+        {
+            SceneManager.LoadScene("αTitle");
+        }
+    }
+}
---
+++ Assets/Scripts/TitleControlα.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace victory7
+{
+    public class TitleControlα : MonoBehaviour
+    {
+        bool m_start = false;
+        void Start()
+        {
+            FadeController.Instance.StartFadeIn();
+            GameManager.Instance.StartSet();
+            MapData.Reset();
+        }
+
+        void Update()
+        {
+            if (m_start)
+            {
+                return;
+            }
+            if (Input.GetButtonDown("Jump"))
+            {
+                GamaStart();
+            }
+        }
+        public void GamaStart()
+        {
+            if (m_start)
+            {
+                return;
+            }
+            m_start = true;
+            FadeController.Instance.StartFadeOut(LoadMap);
+        }
+        void LoadMap()
+        {
+            SceneManager.LoadScene("MapScene");
+        }
+    }
+}
