8f276928b63bee612da547a540a3a6ee3cd9857b
sorude26 <sorude26@gmail.com>
2021-11-26 23:56:25 +0900

    パラメータの最大HPUPの機能を追加した

--- Assets/Scripts/Character/PlayerData.cs
+++ Assets/Scripts/Character/PlayerData.cs
@@ -6,11 +6,13 @@ namespace victory7
 {
     public class PlayerData
     {
+        public static int AdditionHP { get; protected set; }
         public static int CurrentHP { get; protected set; }
         public static int CurrentSP { get; protected set; }
         public static int CurrentGP { get; protected set; }
         public static void StartSet(PlayerParameter parameter)
         {
+            AdditionHP = 0;
             CurrentHP = parameter.MaxHP;
             CurrentSP = 0;
             CurrentGP = 0;
@@ -20,6 +22,10 @@ namespace victory7
             CurrentHP = hp;
             CurrentSP = sp;
             CurrentGP = 0;
+        }
+        public static void AddMaxHp(int addHP)
+        {
+            AdditionHP += addHP;
         }
     }
 }
