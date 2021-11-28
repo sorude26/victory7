259195f7995c3047eacb8b8bd1fa1f18869273aa
sorude26 <sorude26@gmail.com>
2021-11-28 15:14:30 +0900

    スロットレベルアップ機能を追加した

---
+++ Assets/Scripts/Map/MapEvent/LevelUpControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace victory7
+{
+    public class LevelUpControl : MapEventControlBase
+    {
+        public override void SelectAction()
+        {
+            if (!m_select)
+            {
+                return;
+            }
+            if (LevelUp())
+            {
+                OutSelectEvent();
+            }
+        }
+        public bool LevelUp()
+        {
+            bool levelUp = SlotData.LevelUpSlot(m_lineData[m_selectNum].SelectNum, m_selectNum);
+            if (!levelUp)
+            {
+                return false;
+            }
+            m_lineData[0].SetLine(SlotData.LeftSlotData);
+            m_lineData[1].SetLine(SlotData.CenterSlotData);
+            m_lineData[2].SetLine(SlotData.RightSlotData);
+            Select();
+            return true;
+        }
+    }
+}
