9e1386b183d5393dcb27d050e0d7dd5988b394eb
sorude26 <sorude26@gmail.com>
2021-12-05 16:42:34 +0900

    演出用スクリプトを追加した

---
+++ Assets/Scripts/SlotMachine/LineControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineControl : MonoBehaviour
+{
+    [SerializeField]
+    Animator[] m_lines = new Animator[5];
+    bool[] m_view = new bool[5];
+    public void PlayLine(int number)
+    {
+        m_view[number] = true;
+        m_lines[number].Play("alignSlots");
+        SoundManager.Play(SEType.Jackpot);
+    }
+    public void EndView()
+    {
+        for (int i = 0; i < m_lines.Length; i++)
+        {
+            if (m_view[i])
+            {
+                m_view[i] = false;
+                m_lines[i].Play("close");
+            }
+        }
+    }
+}
