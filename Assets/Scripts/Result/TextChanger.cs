91e10b3790f20279bbdaf61ac5171c43299eefba
Ryusei1115 <tekunikaru22@gmail.com>
2022-01-22 16:27:19 +0900

    入力を受け付ける処理を追加した

--- Assets/Scripts/Result/TextChanger.cs
+++ Assets/Scripts/Result/TextChanger.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
+using System.Threading.Tasks;
 
 namespace victory7
 { 
     public class TextChanger : MonoBehaviour
     {
-        [Header("表示したいスコア")]
-        [SerializeField] Text m_scoreText = default;
+        Text m_scoreText = default;
 
         [Header("表示したいスコア名")]
         [SerializeField] string m_scoreName = default;
@@ -21,73 +21,79 @@ namespace victory7
         [Header("受け取りたい変数名")]
         [SerializeField] string m_takeOverName = default;
 
+        [Header("変わる数")]
+        [SerializeField] int m_changeScore = default;
+
         int m_myScore = default;//変数を受け取り反映する変数
 
         const int m_maxScore = 999999999;
 
-        readonly int m_test00 = 100;//これらの変数に入力された値を入れる
-        readonly int m_test01 = default;
-        readonly int m_test02 = default;
-        readonly int m_test03 = default;
+        readonly int m_test00 = default;
 
         //float m_scoreText = default;
-        bool m_start = default; 
+
+        static int m_mapData;
+        int m_victoryCount;
+        int m_sevenSlot;
+
+        
+        bool m_start = default;
+
+        bool m_jump = default;
 
         void Start()
         {
+            m_scoreText = GetComponent<Text>();
+
+            m_mapData = MapData.ClearStageCount;
+
+            m_victoryCount = MapData.ClearStageCount;
+
+            m_sevenSlot = SlotData.SevenSlotData.Count;
+
             //FadeController.Instance.StartFadeIn(() => m_start = true);
 
-            if (m_test00.ToString() == m_takeOverName)
+            if ((nameof(m_mapData)) == m_takeOverName)
+            {
+                m_myScore = m_mapData; 
+            }   
+            else if((nameof(m_victoryCount)) == m_takeOverName)
+            {
+                m_myScore = m_victoryCount;
+            }
+            else if((nameof(m_sevenSlot)) == m_takeOverName)
+            {
+                m_myScore = m_sevenSlot;
+            }
+            else if((nameof(m_test00)) == m_takeOverName)
             {
                 m_myScore = m_test00;
-                Debug.Log("z"); 
             }
-            else if(m_test01.ToString() == m_takeOverName)
-            {
-                m_myScore = m_test01;
-            }
-            else if(m_test02.ToString() == m_takeOverName)
-            {
-                m_myScore = m_test02;
-            }
-            else if(m_test03.ToString() == m_takeOverName)
-            {
-                m_myScore = m_test03;
-            }
-
-            AddScore(m_myScore);
+            Debug.Log(m_myScore);
         }
 
-        void Update()
+        async void Update()
         {
-            if (m_start && Input.GetButtonDown("Jump"))
+            if (!m_jump && Input.GetButtonDown("Jump"))
+            {
+                AddScore();
+                await Task.Delay(1000);
+                m_jump = true;
+            }
+            if(m_jump && Input.GetButtonDown("Jump"))
             {
                 ReturnTitle();
             }
         }
 
-        public void AddScore(int score)
+        public void AddScore()
         {
-            int tempScore = m_myScore;
-            m_myScore = Mathf.Min(m_myScore + score, m_maxScore);
-
-            if(tempScore != m_maxScore)
-            {
-                DOTween.To(() => tempScore,
-                    x => tempScore = x,
-                    m_myScore,
-                    m_scoreChangeInterval)
-                    .OnUpdate(() => m_scoreText.text = tempScore.ToString(m_scoreName + "     " + "00000"))
-                    .OnComplete(() => m_scoreText.text = m_myScore.ToString(m_scoreName + "     " + "00000"));
-            }
+            m_scoreText.DOText(m_scoreName + "     " + m_myScore.ToString(),m_scoreChangeInterval);
         }
 
         public void ReturnTitle()
         {
-            if (!m_start)
-            {
-                return;
-            }
+            if (!m_start) return;
             m_start = false;
             FadeController.Instance.StartFadeOut(LoadTitle);
         }
