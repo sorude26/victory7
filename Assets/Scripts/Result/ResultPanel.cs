d3d16a69837221e466fcc6bcd3b21a093bb3284e
Ryusei1115 <tekunikaru22@gmail.com>
2022-02-20 18:27:18 +0900

    リザルト画面作業中

---
+++ Assets/Scripts/Result/ResultPanel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using victory7;
+
+namespace victory7
+{
+    public class ResultPanel : MonoBehaviour
+    {
+        [SerializeField]
+        RectTransform[] m_line = default;
+        [SerializeField]
+        float m_slotScale = 1.5f;
+
+        List<Slot> m_leftData;
+        List<Slot> m_centerData;
+        List<Slot> m_rightData;
+
+        bool m_jump = false;
+
+        void Start()
+        {
+            m_leftData = SlotData.LeftSlotData;
+            m_centerData = SlotData.CenterSlotData;
+            m_rightData = SlotData.RightSlotData;
+        }
+
+        void Update()
+        {
+            if(Input.GetButtonDown("Jump") && !m_jump)
+            {
+                m_jump = true;
+                Debug.Log("jump");
+                SetResultSlot();
+            }
+        }
+        void SetResultSlot()
+        {
+            foreach (var slot in m_leftData)
+            {
+                var data = Instantiate(slot, m_line[0]);
+                data.transform.position = m_line[0].position;
+            }
+            foreach (var slot in m_centerData)
+            {
+                var data = Instantiate(slot, m_line[1]);
+                data.transform.position = m_line[1].position;
+
+            }
+            foreach (var slot in m_rightData)
+            {
+                var data = Instantiate(slot, m_line[2]);
+                data.transform.position = m_line[2].position;
+
+            }
+        }
+    }
+}
--- Assets/Scripts/Result/TextChanger.cs
+++ Assets/Scripts/Result/TextChanger.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
-using System.Threading.Tasks;
 
 namespace victory7
 { 
     public class TextChanger : MonoBehaviour
     {
-        Text m_scoreText = default;
+        Text m_scoreText;
 
         [Header("表示したいスコア名")]
         [SerializeField] string m_scoreName = default;
@@ -21,55 +20,50 @@ namespace victory7
         [Header("受け取りたい変数名")]
         [SerializeField] string m_takeOverName = default;
 
-        [Header("変わる数")]
-        [SerializeField] int m_changeScore = default;
-
         int m_myScore = default;//変数を受け取り反映する変数
 
         readonly int m_test00 = default;
 
-        //float m_scoreText = default;
-
-        int m_mapData;
+        int m_ClearStageCountData;
         int m_victoryCount;
         int m_sevenSlot;
 
         
         bool m_start = default;
 
+        bool m_flag = default;
+
         bool m_jump = default;
 
-        bool m_flag = default;
 
         void Start()
         {
             m_scoreText = GetComponent<Text>();
 
-            m_mapData = MapData.ClearStageCount;
+            m_ClearStageCountData = MapData.ClearStageCount;
 
-            m_victoryCount = MapData.ClearStageCount;
+            m_victoryCount = MapData.BattleCount;
 
-            m_sevenSlot = SlotData.SevenSlotData.Count;
+            m_sevenSlot = SlotData.SevenSlotCount();
 
             FadeController.Instance.StartFadeIn(() => m_start = true);
 
-            if ((nameof(m_mapData)) == m_takeOverName)
+            if (nameof(m_ClearStageCountData) == m_takeOverName)
             {
-                m_myScore = m_mapData; 
+                m_myScore = m_ClearStageCountData; 
             }   
-            else if((nameof(m_victoryCount)) == m_takeOverName)
+            else if(nameof(m_victoryCount) == m_takeOverName)
             {
                 m_myScore = m_victoryCount;
             }
-            else if((nameof(m_sevenSlot)) == m_takeOverName)
+            else if(nameof(m_sevenSlot) == m_takeOverName)
             {
                 m_myScore = m_sevenSlot;
             }
-            else if((nameof(m_test00)) == m_takeOverName)
+            else if(nameof(m_test00) == m_takeOverName)
             {
                 m_myScore = m_test00;
             }
-            Debug.Log(m_myScore);
         }
 
         void Update()
@@ -86,21 +80,23 @@ namespace victory7
             }
         }
 
-        public void AddScore()
+        void AddScore()
         {
             m_scoreText.DOText(m_scoreName + "     " + m_myScore.ToString(),m_scoreChangeInterval);
         }
 
-        public void ReturnTitle()
+        void ReturnTitle()
         {
             if (!m_start) return;
             m_start = false;
             FadeController.Instance.StartFadeOut(LoadTitle);
         }
+
         void LoadTitle()
         {
             SceneManager.LoadScene("αTitle");
         }
+
         IEnumerator WaitInput()
         {
             yield return new WaitForSeconds(1);
