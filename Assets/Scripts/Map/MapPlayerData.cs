942c6f1749160c83d0be113d69bc300b51e01e84
sorude26 <sorude26@gmail.com>
2022-03-20 18:18:19 +0900

    HP表示を追加した

---
+++ Assets/Scripts/Map/MapPlayerData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace victory7
+{
+    public class MapPlayerData : MonoBehaviour
+    {
+        [SerializeField]
+        Slider m_hpSlider = default;
+        [SerializeField]
+        Slider m_spSlider = default;
+        void Start()
+        {
+            DataUpdate();
+        }
+        public void DataUpdate()
+        {
+            m_hpSlider.maxValue = PlayerData.MaxHP;
+            m_hpSlider.value = PlayerData.CurrentHP;
+            m_spSlider.maxValue = PlayerData.MaxSP;
+            m_spSlider.value = PlayerData.CurrentSP;
+        }
+    }
+}
